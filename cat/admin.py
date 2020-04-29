3bf43979bb1a29e3b1bdec0001dc0e263224b7cb
lesenelir <miaomiaobabyzy@gmail.com>
2020-04-29 12:59:42 +0800

    delete

--- cat/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# 自定义管理页面
-class CatinfoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'nameinfo', 'feature', 'livemethod', 'feednn', 'feedmethod']
-
-# Register your models here.
-admin.site.register(Catinfo, CatinfoAdmin)
--- cat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from cat import views
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^info/', views.catinfo),
-]
